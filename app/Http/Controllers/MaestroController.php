c5d33bd08f8aa723042860951078cc93c2e6e873
Cristian Hernandez <utp0154256@alumno.utpuebla.edu.mx>
2023-08-21 14:33:14 -0600

    Commit Casi Listo

--- app/Http/Controllers/MaestroController.php
+++ app/Http/Controllers/MaestroController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Maestro;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class MaestroController extends Controller
 {
@@ -12,7 +13,8 @@ class MaestroController extends Controller
      */
     public function index()
     {
-        //
+        $maestros = Maestro::all();
+        return view('maestro.index',compact('maestros'));
     }
 
     /**
@@ -20,7 +22,7 @@ class MaestroController extends Controller
      */
     public function create()
     {
-        //
+        return view('maestro.create');
     }
 
     /**
@@ -28,7 +30,27 @@ class MaestroController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $maestros = new Maestro();
+
+        $request->validate([
+            'codigo'=>['required','unique:maestros,codigo','min:10','max:10','numeric','integer'],
+            'nombre'=>['required','alpha_num:ascii'],
+            'apellidopaterno'=>['required','alpha_num:ascii'],
+            'apellidomaterno'=>['required','alpha_num:ascii'],
+            'NSS'=>['required','unique:maestros,NSS','min:12','max:12','numeric','integer'],
+            'correo'=>['required','unique:maestros,correo','email:rfc,dns']
+        ]);
+
+        $maestros->codigo = $request->get('codigo');
+        $maestros->nombre = $request->get('nombre');
+        $maestros->apellidopaterno = $request->get('apellidopaterno');
+        $maestros->apellidomaterno = $request->get('apellidomaterno');
+        $maestros->NSS = $request->get('NSS');
+        $maestros->correo = $request->get('correo');
+
+        $maestros->save();
+
+        return redirect('/maestros');
     }
 
     /**
@@ -36,7 +58,7 @@ class MaestroController extends Controller
      */
     public function show(Maestro $maestro)
     {
-        //
+        
     }
 
     /**
@@ -44,7 +66,8 @@ class MaestroController extends Controller
      */
     public function edit(Maestro $maestro)
     {
-        //
+        $maestro = Maestro::find($id);
+        return view('maestro.edit',compact('maestro'));
     }
 
     /**
@@ -52,7 +75,28 @@ class MaestroController extends Controller
      */
     public function update(Request $request, Maestro $maestro)
     {
-        //
+
+        $maestro = Maestro::find($id);
+
+        $request->validate([
+            'codigo'=>['required','unique:maestros,codigo','min:10','max:10','numeric','integer'],
+            'nombre'=>['required','alpha_num:ascii'],
+            'apellidopaterno'=>['required','alpha_num:ascii'],
+            'apellidomaterno'=>['required','alpha_num:ascii'],
+            'NSS'=>['required','unique:maestros,NSS','min:12','max:12','numeric','integer'],
+            'correo'=>['required',Rule::unique('estudiante','correo')->ignore($id),'email:rfc,dns']
+        ]);
+
+        $maestro->codigo = $request->get('codigo');
+        $maestro->nombre = $request->get('nombre');
+        $maestro->apellidopaterno = $request->get('apellidopaterno');
+        $maestro->apellidomaterno = $request->get('apellidomaterno');
+        $maestro->NSS = $request->get('NSS');
+        $maestro->correo = $request->get('correo');
+
+        $maestro->save();
+
+        return redirect('/maestros');
     }
 
     /**
@@ -60,6 +104,8 @@ class MaestroController extends Controller
      */
     public function destroy(Maestro $maestro)
     {
-        //
+        $maestro = Maestro::find($id);
+        $maestro->delete();
+        return redirect('/maestros');
     }
 }
